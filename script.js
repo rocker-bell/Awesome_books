90cb60611cdfb87a9fb950a1c39795d6a5d353aa
Anasscodes <rockbell8@gmail.com>
2021-07-12 18:02:59 +0300

    Add changes

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
+// class Book {
+//     constructor({Title, Author, BookId}) {
+//         this.Title = Title;
+//         this.Author = Author;
+//         this.BookId = BookId;
+//     }
+// }
+
 let Books = [];
 
-function AddBook(Book) {
-    
+function AddBook() {
     const Title = document.getElementById('title');
     const Author = document.getElementById('author');
     const PTitle = document.createElement('p');
     const PAuthor = document.createElement('p');
+    PTitle.innerHTML = 'Title: ${Book.title}';
+    PAuthor.innerHTML = 'Author: ${Book.author}';
+    const RButton = document.createElement('button');
+    RButton.setAttribute('id', 'Remove')
     PTitle.appendChild(Title.value);
     PAuthor.appendChild(Author.value);
     const BooksCollection = document.createElement('div');
-    BooksCollection.appendChild(PTitle).appendChild(PAuthor);
-    Books.push(BooksCollection);
-    
-
-
+    BooksCollection.appendChild(PTitle).appendChild(PAuthor).appendChild(RButton);
+    Books.push(Book);
+    console.log(BooksCollection)
 }
 
-const Add = document.getElementById('add');
-Add.addEventListener ('click', AddBook());
+
+
