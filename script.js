993be72cfb8c295be60331bd9d5e07ab79872ba0
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-07-13 14:22:42 +0200

    Add eventListeners

--- script.js
+++ script.js
@@ -1,29 +1,53 @@
-// class Book {
-//     constructor({Title, Author, BookId}) {
-//         this.Title = Title;
-//         this.Author = Author;
-//         this.BookId = BookId;
-//     }
-// }
+const library = document.querySelector('#library');
 
-let Books = [];
+//add books
+const addBooks = document.forms['addBook'];
 
-function AddBook() {
-    const Title = document.getElementById('title');
-    const Author = document.getElementById('author');
-    const PTitle = document.createElement('p');
-    const PAuthor = document.createElement('p');
-    PTitle.innerHTML = 'Title: ${Book.title}';
-    PAuthor.innerHTML = 'Author: ${Book.author}';
-    const RButton = document.createElement('button');
-    RButton.setAttribute('id', 'Remove')
-    PTitle.appendChild(Title.value);
-    PAuthor.appendChild(Author.value);
-    const BooksCollection = document.createElement('div');
-    BooksCollection.appendChild(PTitle).appendChild(PAuthor).appendChild(RButton);
-    Books.push(Book);
-    console.log(BooksCollection)
-}
+let bookLib = [];
 
+addBooks.addEventListener('submit', function(e){
+  e.preventDefault();
+  const book = addBooks.querySelector('input[id="bookTitle"]').value;
+  const author = addBooks.querySelector('input[id="authorName"]').value;
+  
+  bookLib.push({
+   bookName: book,
+   bookAuthor: author,
+  });
+  
+  localStorage.setItem("books", JSON.stringify(bookLib));
+ 
+  //create elements
+  const list = document.createElement('li');
+  const bookName = document.createElement('span');
+  const authorName = document.createElement('span');
+  const deleteButton = document.createElement('button');
+
+  // add text
+  bookName.textContent = book;
+  authorName.textContent = author;
+  deleteButton.textContent = 'Remove';
+
+  //remove books
+
+  library.addEventListener('click', function(e){
+    if(e.target.className === 'delete') {
+      const book = e.target.parentElement;
+      library.removeChild(book);
+    };
+  });
+
+  //add class
+  bookName.classList.add('bookTitle');
+  authorName.classList.add('authorName');
+  deleteButton.classList.add('delete');
+
+  //add to elements
+  list.appendChild(bookName);
+  list.appendChild(authorName);
+  list.appendChild(deleteButton);
+  library.appendChild(list);
+  library.style.listStyleType = 'none';
+});
 
 
