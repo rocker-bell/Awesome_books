b2fc5f1ba0334cc839fcf37205f7b911c1c4c894
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-07-14 16:11:39 +0200

    Add heading to html and a function to retrieve books

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-//create a class
+//create a class with book details
+class Book {
+  constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
+}
+
+//Class with list of books
 class BookList {
   constructor(Books) {
     this.bookListCollection = Books;
   }
 
+  //Add book list to the top of the page
   addBook(Book) {
     this.bookListCollection.push(Book);
     const TitleP = document.createElement('p');
@@ -21,7 +31,8 @@ class BookList {
     document.getElementById('list_container').appendChild(BookContainer).appendChild(AuthorP);
     document.getElementById('list_container').appendChild(BookContainer).appendChild(RButton);
   }
-
+  
+  //Remove books from the page when the button is clicked
   RemoveBook(BookId) {
     const BookContainer = document.getElementById(BookId);
     BookContainer.parentNode.removeChild(BookContainer);
@@ -40,10 +51,12 @@ class BookList {
     localStorage.setItem('library', BookList);
   }
 
+  //Adds books to local storage 
   AddToStorage() {
     localStorage.setItem('library', JSON.stringify(this.bookListCollection));
   }
-
+  
+  //Displays books
   ShowBooks() {
     for (const i in this.bookListCollection) {
       this.addBook(this.bookListCollection[i])
@@ -51,18 +64,10 @@ class BookList {
   }
 }
 
-class Book {
-  constructor(title, author, id) {
-    this.title = title;
-    this.author = author;
-    this.id = id;
-  }
-}
-
-
-
+//Retrieves books from library
 const NewBookCollection = new BookList(JSON.parse(localStorage.getItem('library')) || []);
 NewBookCollection.ShowBooks();
+
 function AddNewbook() {
   const title = document.getElementById('Title');
   const author = document.getElementById('Author');
